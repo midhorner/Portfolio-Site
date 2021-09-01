66e9633d4e56c4cd20de218660e08c36d9be1188
midhorner <horner.andrew.e@gmail.com>
2021-09-01 14:58:28 -0400

    finished content for projects section

--- js/index.js
+++ js/index.js
@@ -13,12 +13,21 @@ const LINKS = [
   "https://en.wikipedia.org/wiki/Aristotle",
   "https://en.wikipedia.org/wiki/Ludwig_Wittgenstein",
   "https://en.wikipedia.org/wiki/Beck",
+  "https://en.wikipedia.org/wiki/Noam_Chomsky",
+  "https://en.wikipedia.org/wiki/Ted_Lasso",
+  "https://en.wikipedia.org/wiki/The_Good_Place",
+  "https://en.wikipedia.org/wiki/Beer",
+  "https://en.wikipedia.org/wiki/Calvin_and_Hobbes",
+  "https://en.wikipedia.org/wiki/Ori_and_the_Blind_Forest",
+  "https://en.wikipedia.org/wiki/Wok",
+  "https://en.wikipedia.org/wiki/Stardew_Valley",
+  "https://en.wikipedia.org/wiki/Ishmael_(Quinn_novel)",
+  "https://en.wikipedia.org/wiki/Pinkerton_(album)",
+  "https://en.wikipedia.org/wiki/The_Hustler_(film)",
 ];
 
 const randomLink = () => {
   let i = Math.floor(Math.random() * LINKS.length);
-  console.log(i + " " + LINKS.length);
-  console.log(LINKS[i]);
   return LINKS[i];
 };
 
